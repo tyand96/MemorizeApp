096c9ab04f4943af586ab7a06b70cbfea6c94744
Tyler Anderson <tyand96@gmail.com>
2023-07-21 16:00:18 -0500

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Tyler Anderson on 7/21/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
